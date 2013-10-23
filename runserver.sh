b5501b55f9e33b27787da92651e217edb6726c09
Lorenzo Setale <koalalorenzo@gmail.com>
2013-10-23 20:55:25 +0200

    Better output from runserver script

--- runserver.sh
+++ runserver.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+echo -n "Killing old process: "
+screen -S togethernetwork -X quit &> /dev/null
+echo "done"
+
+echo -n "Upgrading dependencies inside the virtual environment: "
 source venv/bin/activate
 cd togethernetwork
 pip install -U -r requirements.txt
+echo "done"
 
-screen -S togethernetwork -X quit &> /dev/null
-echo "Sleeping 1 second..."
+echo "Sleeping 1 second before creating a new screen..."
 sleep 1
 screen -S togethernetwork ./runserver.sh
