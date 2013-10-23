204a3fe06cec3b089393f9397c0da2ee27560276
Lorenzo Setale <koalalorenzo@gmail.com>
2013-10-23 21:31:42 +0200

    Renamed Homepage component in Mainpaegs

---
+++ togethernetwork/Mainpages/urls.py
@@ -0,0 +1,8 @@
+# -*- coding=utf-8 -*-
+
+from django.conf.urls import patterns, url
+from . import views
+
+urlpatterns = patterns('',
+    url(r'^', views.homepage_view, name='homepage_view'), 
+)
