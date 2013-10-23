353445f03eaa0fe548e3fe878d306c65d0512f2a
Lorenzo Setale <koalalorenzo@gmail.com>
2013-10-23 20:52:08 +0200

    Better description from init script.

--- init.sh
+++ init.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+echo -n "Cleaning & updating the local copy: "
+git fetch --all >> /dev/null
+git reset --hard origin/master  >> /dev/null
+git pull >> /dev/null
+echo "done"
 
-virtualenv venv -v
+echo -n "Creating a virtual environment: "
+virtualenv venv -v >> /dev/null
+echo "done"
+
+echo -n "Installing dependencies inside the virtual environment: "
 source venv/bin/activate
 pip install -U -r requirements.txt
+echo "done"
