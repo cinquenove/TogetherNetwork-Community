4fae04269aa3ce86c2fbf0fa485449c28d93c22f
Lorenzo Setale <koalalorenzo@gmail.com>
2013-10-28 18:26:43 +0100

    Connected correctly about view with the correct URL

--- togethernetwork/Mainpages/urls.py
+++ togethernetwork/Mainpages/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^', views.homepage_view, name='homepage_view'), 
-    url(r'^about', views.homepage_view, name='homepage_view'), 
+    url(r'^about', views.about_view, name='about_view'), 
 )
 
 
