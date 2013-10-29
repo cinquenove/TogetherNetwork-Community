012f1fcb7738dbf2315685d2f5270f90216e89a6
Lorenzo Setale <koalalorenzo@gmail.com>
2013-10-29 12:40:13 +0100

    Fix upload path

--- togethernetwork/togethernetwork/settings.py
+++ togethernetwork/togethernetwork/settings.py
@@ -55,7 +55,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
