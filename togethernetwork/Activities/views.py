c50654c998dc3791b86ac35aaa50b773a99e487a
Ernesto Cinquenove <ernestocinquenove@gmail.com>
2013-10-29 12:11:01 +0100

    Implemented view for activities

--- togethernetwork/Activities/views.py
+++ togethernetwork/Activities/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+# -*- coding=utf-8 -*-
+from django.shortcuts import render_to_response
+
+from django.template import RequestContext
+
+def activities_view(request):
+    """
+        List of activities.
+    """
+    return render_to_response("activities.html", context_instance=RequestContext(request))
+
+
+
