79d4d54494dcaa596834ecb84dcd9caa65bd3318
Lorenzo Setale <koalalorenzo@gmail.com>
2013-10-29 15:20:55 +0100

    Implementing the activity view via python: list of activities

--- togethernetwork/Activities/views.py
+++ togethernetwork/Activities/views.py
@@ -1,13 +1,15 @@
 # -*- coding=utf-8 -*-
 from django.shortcuts import render_to_response
-
 from django.template import RequestContext
+
+from .models import Activity
 
 def activities_view(request):
     """
         List of activities.
     """
-    return render_to_response("activities.html", context_instance=RequestContext(request))
+    activities = Activity.objects.all()
+    return render_to_response("activities.html", {"activities": activities }, context_instance=RequestContext(request))
 
 
 
