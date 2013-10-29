173d02901029bee00017c7251f21770356a8b103
Lorenzo Setale <koalalorenzo@gmail.com>
2013-10-29 18:35:41 +0100

    Importing redirect

--- togethernetwork/Activities/views.py
+++ togethernetwork/Activities/views.py
@@ -1,5 +1,6 @@
 # -*- coding=utf-8 -*-
 from django.shortcuts import render_to_response
+from django.shortcuts import redirect
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
