334dc25ba4a1d1fcf417bc94fa17ce158adcd661
Lorenzo Setale <koalalorenzo@gmail.com>
2013-10-29 11:39:44 +0100

    Created Activity model forms

---
+++ togethernetwork/Activities/form.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm
+from .models import Activity
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Activity
+        fields = [
+            #"owner",
+            "title",
+            "description",
+            "activity_type",
+
+            "time",
+            "location",
+            "photo",
+
+            "attendee_limit",
+            #"attendee"
+        ]
