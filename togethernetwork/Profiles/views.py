5cc3756becb9a125c1393cb275f0212ba5497f83
Lorenzo Setale <koalalorenzo@gmail.com>
2013-10-31 12:22:36 +0100

    Created a profile_view function to handle the view for each profile

--- togethernetwork/Profiles/views.py
+++ togethernetwork/Profiles/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+# -*- coding=utf-8 -*-
+from django.shortcuts import render_to_response
+from django.shortcuts import redirect
+from django.template import RequestContext
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.models import User
+from Activities.models import Activity
+
+from .models import Profile
+
+def profile_view(request, profile_pk=None):
+    """
+        Show a single profile
+    """
+    profile = None
+    if profile_pk:
+        profile = get_object_or_404(Profile, pk=profile_pk)
+
+    return render_to_response("profile.html", 
+        {"profile": profile }, 
+        context_instance=RequestContext(request))
