6e846a9c180833dd640e85d637568c5ab0f9c9ec
Lorenzo Setale <koalalorenzo@gmail.com>
2013-10-31 15:47:47 +0100

    Enabled edit_profile_view to edit Profiles

--- togethernetwork/Profiles/urls.py
+++ togethernetwork/Profiles/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^(?P<profile_pk>\d+)$', views.profile_view, name='profile_view'),
-    url(r'^nobody$', views.profile_view, name='profile_view'),
+    url(r'^edit$', views.edit_profile_view, name='edit_profile_view'),
 )
 
 
--- togethernetwork/Profiles/views.py
+++ togethernetwork/Profiles/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 from Activities.models import Activity
 
 from .models import Profile
+from .forms import ProfileForm
 
 def profile_view(request, profile_pk=None):
     """
@@ -16,8 +17,36 @@ def profile_view(request, profile_pk=None):
     """
     profile = None
     if profile_pk:
-        profile = get_object_or_404(Profile, pk=profile_pk)
+        profile = Profile.objects.get(pk=profile_pk)
 
     return render_to_response("profile.html", 
         {"profile": profile }, 
         context_instance=RequestContext(request))
+
+@login_required
+def edit_profile_view(request):
+    """
+        Edit the Profile
+    """
+    form_title = "My Profile"
+
+    try:
+        profile = Profile.objects.get(owner=request.user)
+    except:
+        profile = None
+
+    if request.method == 'POST':
+        formset = ProfileForm(request.POST, request.FILES, instance=profile)
+        if formset.is_valid():
+            profile_obj = formset.save(commit=False)
+            
+            profile_obj.owner = request.user
+            profile_obj.save()
+            return redirect(profile_obj)
+    else:
+        formset = ProfileForm(instance=profile)
+        
+    return render_to_response("form.html", {
+        "form": formset,
+        "title": form_title
+    }, context_instance=RequestContext(request))
