d5ac144433caa4ffac0123de043ed44356bd63fb
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-05 15:05:19 +0100

    Implemented Booking, Acommodation and AccommodationPhoto object.

--- togethernetwork/Accommodations/models.py
+++ togethernetwork/Accommodations/models.py
@@ -13,43 +13,93 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 
 from datetime import datetime, timedelta
 
-ACCOMMODATION_TYPE = [
-    ('MOR', '4+ beds'),
-    ('QUD', '4 beds'),
-    ('TRP', '3 beds'),
-    ('DOU', '2 beds'),
-    ('SNG', '1 bed'),
+ROOM_SIZES = [
+    ('FPD', '4 persons dorm'),
+    ('DOU', 'Double'),
+    ('SNG', 'Single'),
 ]
 
-BOOKING_STATUSES = [
-    ('ACC', 'Accepted'),
-    ('WFA', 'Waiting for approval'),
-    ('WFP', 'Waiting for payment'),
-    ('DEC', 'Declined'),
+ROOM_TYPES [
+    ('LCL', 'Local'),
+    ('GST', 'Guest'),
 ]
 
 def get_accommodations_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
-    return os.path.join("accommodations", filename)
+    return os.path.join("Accommodations", "main_photo", filename)
 
 class Accommodation(models.Model):
     """
         A single Accommodation.
     """
-    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
     description = models.TextField(max_length=500, default="")
-    accommodation_type = models.CharField(max_length=3, choices=ACCOMMODATION_TYPE)
-    photo = models.ImageField(upload_to=get_accommodations_path, blank=True, null=True)
+    room_size = models.CharField(max_length=3, choices=ROOM_SIZES)
+    room_type = models.CharField(max_length=3, choices=ROOM_TYPES)
+    main_photo = models.ImageField(upload_to=get_accommodations_path, blank=True, null=True)
 
-    price = models.FloatField(default=0.0) 
+    price_per_day = models.FloatField(default=0.0) 
+    price_per_month = models.FloatField(default=0.0) 
 
     def create_thumbnails(self):
-        # original code for this method came from
-        # http://snipt.net/danfreak/generate-thumbnails-in-django-with-pil/
+        if not self.main_photo:
+            return
 
-        # If there is no image associated with this.
-        # do not create thumbnail
+        THUMBNAIL_BIG_SIZE = (1200,1200)
+
+        DJANGO_TYPE = self.main_photo.file.name.split("/")[-1].split(".")[-1].lower()
+        if DJANGO_TYPE in ['jpeg', 'jpg']:
+            PIL_TYPE = 'jpeg'
+            FILE_EXTENSION = 'jpg'
+        elif DJANGO_TYPE in ['png']:
+            PIL_TYPE = 'png'
+            FILE_EXTENSION = 'png'
+
+        file_read = self.main_photo.read()
+        image_big = Image.open(StringIO(file_read))
+        image_big.thumbnail(THUMBNAIL_BIG_SIZE, Image.ANTIALIAS)
+
+        # Save the thumbnail
+        temp_handle_big = StringIO()
+        image_big.save(temp_handle_big, PIL_TYPE)
+        temp_handle_big.seek(0)
+
+        # Save image to a SimpleUploadedFile which can be saved into
+        # ImageField
+        suf_big = SimpleUploadedFile(os.path.split(self.main_photo.name)[-1],
+             temp_handle_big.read(), content_type=DJANGO_TYPE)
+        
+        # Save SimpleUploadedFile into image field
+        self.main_photo.save('%s_big.%s'%(os.path.splitext(suf_big.name)[0],FILE_EXTENSION), suf_big, save=False)
+        
+    def save(self):
+        # create a thumbnail
+        self.create_thumbnails()
+        super(Accommodation, self).save()
+
+    def get_absolute_url(self):
+        return "/accommodations/%s" % self.pk
+
+    def __str__(self):
+        return "%s" % self.title
+
+## AccommodationPhoto
+
+def get_accommodation_photos_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join("Accommodations", "photos", filename)
+
+class AccommodationPhoto(models.Model):
+    """
+        A single Photo of a single Accommodation.
+    """
+    title = models.CharField(blank=True, default="")
+    photo = models.ImageField(upload_to=get_accommodation_photos_path, blank=True, null=True)
+    accommodation = models.ForeignKey(Accommodation, related_name="booking_accommodation")
+
+    def create_thumbnails(self):
         if not self.photo:
             return
 
@@ -67,20 +117,6 @@ class Accommodation(models.Model):
         # Open original photo which we want to thumbnail using PIL's Image
         file_read = self.photo.read()
         image_big = Image.open(StringIO(file_read))
-
-        # Convert to RGB if necessary
-        # Thanks to Limodou on DjangoSnippets.org
-        # http://www.djangosnippets.org/snippets/20/
-        #
-        # I commented this part since it messes up my png files
-        #
-        #if image.mode not in ('L', 'RGB'):
-        #    image = image.convert('RGB')
-
-        # We use our PIL Image object to create the thumbnail, which already
-        # has a thumbnail() convenience method that contrains proportions.
-        # Additionally, we use Image.ANTIALIAS to make the image look better.
-        # Without antialiasing the image pattern artifacts may result.
         image_big.thumbnail(THUMBNAIL_BIG_SIZE, Image.ANTIALIAS)
 
         # Save the thumbnail
@@ -107,14 +143,23 @@ class Accommodation(models.Model):
     def __str__(self):
         return "%s" % self.title
 
+## Booking 
+
+BOOKING_STATUSES = [
+    ('ACC', 'Accepted'),
+    ('WFA', 'Waiting for approval'),
+    ('WFP', 'Waiting for payment'),
+    ('DEC', 'Declined'),
+]
+
 class Booking(models.Model):
     """
-        The Booking for a period of time owned by a user for a specific Accommodation.
+        The Booking for a period of time "owned" by a user for a specific Accommodation.
     """
-    start = models.DateField(default=( datetime.now() + timedelta(days=1) ) )
-    stop = models.DateField(default=( datetime.now() + timedelta(days=2) ) )
+    chackin_date = models.DateField(default=( datetime.now() + timedelta(days=1) ) )
+    chackout_date = models.DateField(default=( datetime.now() + timedelta(days=2) ) )
 
-    status = models.CharField(max_length=3, choices=BOOKING_STATUSES)
+    status = models.CharField(max_length=3, default="WFA", choices=BOOKING_STATUSES)
     tenant = models.ForeignKey(User, related_name="booking_tenant")
     accommodation = models.ForeignKey(Accommodation, related_name="booking_accommodation")
 
@@ -123,8 +168,8 @@ class Booking(models.Model):
 
     def __str__(self):
         return "(%s)[%s] %s booked by %s" % ( 
-                self.start,
+                self.chackin_date,
                 self.status,
-                self.accommodation.title, 
+                self.accommodation.name, 
                 self.tenant.username 
             )
