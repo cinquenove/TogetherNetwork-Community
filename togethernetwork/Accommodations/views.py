eca4285e20290cdb2fb6c1a71985c7175fcda2ef
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-05 17:14:56 +0100

    Implemented Accommodation list view

--- togethernetwork/Accommodations/views.py
+++ togethernetwork/Accommodations/views.py
@@ -6,15 +6,16 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth.models import User
-
+from .models import Accommodation
 
 
 def accommodations_view(request):
     """
-        List of activities.
+        List of accommodations.
     """
 
-#    accommodations = Accommodation.objects.all()
+    accommodations = Accommodation.objects.all()
     return render_to_response("accommodations.html", 
+        {"accommodations": accommodations},
         context_instance=RequestContext(request))
 
