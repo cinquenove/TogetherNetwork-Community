39d58809cc32e8b5a41d1a469229ac207732ce8b
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-05 12:53:57 +0100

    Added dummy URLs (disabled) for Accommodations

--- togethernetwork/Accommodations/urls.py
+++ togethernetwork/Accommodations/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('', 
+    #url(r'^(?P<activity_pk>\d+)/book$', views.book_accommodation, name='book_accommodation'),
+    #url(r'^(?P<activity_pk>\d+)$', views.single_accommodation, name='single_accommodation'),
+
     url(r'^list', views.accommodations_view, name='accommodations_view'), 
     url(r'^$', views.accommodations_view, name='accommodations_view'), 
 )
