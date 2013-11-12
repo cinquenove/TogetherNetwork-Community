aacf0d55528cc0744a43cca7616e6c6a4f28d887
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-13 00:15:37 +0100

    Using the right path for directory

--- togethernetwork/settings.py
+++ togethernetwork/settings.py
@@ -66,7 +66,7 @@ MEDIA_ROOT = ""
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'http://static.togethernetwork.org.s3-website-eu-west-1.amazonaws.com/'
+MEDIA_URL = 'http://media.togethernetwork.org.s3-website-eu-west-1.amazonaws.com/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
