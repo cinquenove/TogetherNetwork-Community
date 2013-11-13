24f29762ec54a0effee48dfbf09f36ea9d09d3d6
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-13 01:09:11 +0100

    Removed unused files

--- togethernetwork/s3utils.py
+++
@@ -1,4 +0,0 @@
-from storages.backends.s3boto import S3BotoStorage
-
-STATICFILES_STORAGE = lambda: S3BotoStorage(bucket="static.togethernetwork.org")
-DEFAULT_FILE_STORAGE = lambda: S3BotoStorage(bucket="media.togethernetwork.org")
--- togethernetwork/settings.py
+++ togethernetwork/settings.py
@@ -196,8 +196,7 @@ AVATAR_GRAVATAR_BACKUP = True
 
 # Django Storages
 DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
-#DEFAULT_FILE_STORAGE = 'togethernetwork.s3utils.DEFAULT_FILE_STORAGE'
-#STATICFILES_STORAGE = 'togethernetwork.s3utils.STATICFILES_STORAGE'
+STATICFILES_STORAGE = 'storages.backends.s3.S3Storage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
