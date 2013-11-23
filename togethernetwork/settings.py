931ac373cd3b6af29e03ac54e686850a3b448d92
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-23 02:01:54 +0100

    Using PickleSerializer to fix a problem with facebook auth.

--- togethernetwork/settings.py
+++ togethernetwork/settings.py
@@ -165,7 +165,7 @@ INSTALLED_APPS = (
     "Accommodations",
 )
 
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
