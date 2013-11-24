0c25f1e7a6593344b2802dd6762b599039e78c57
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-24 14:01:02 +0100

    impImporting the right module needed to send emails

--- Accommodations/views.py
+++ Accommodations/views.py
@@ -5,9 +5,9 @@ from django.template import RequestContext
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.mail import send_mail, mail_admins
 
 from django.contrib.auth.models import User
-
 from .models import Accommodation
 from .models import AccommodationPhoto
 
