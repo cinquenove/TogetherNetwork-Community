74de20cc855272b90f9443e2641c8f1c88df6ee2
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-24 11:47:10 +0100

    Reset instead of "clean"

--- resetdb.sh
+++ resetdb.sh
@@ -2,13 +2,13 @@
 # -*- coding=utf-8 -*-
 
 if [[ $@ == **sure** ]]; then
-    echo -n "- Cleaning the database: "
-        python manage.py reset_db --noinput
+    echo -n "- Reset database: "
+        heroku pg:reset DATABASE_URL --confirm together-network
     echo "done"
     exit 0
 fi
 
-echo -n "- Cleaning the database: "
+echo -n "- Reset database: "
 echo "failed"
 echo ""
 echo "use 'cleandb.sh sure' if you are sure of what you are doing!"
