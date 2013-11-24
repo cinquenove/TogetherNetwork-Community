bc3bb691ca758b4853fc990c8679bc708bae4bc2
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-24 13:54:25 +0100

    Configured EMAIL to work with SENDGRID

--- togethernetwork/settings.py
+++ togethernetwork/settings.py
@@ -199,6 +199,13 @@ LOGGING = {
     }
 }
 
+# Email
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+
 # Django Messages (integrated)
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
