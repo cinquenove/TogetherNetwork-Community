536ba9c817c4f4d8fa5726721d1866db1c384da4
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-30 22:40:42 +0100

    enabled any kind of username

--- Profiles/urls.py
+++ Profiles/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = patterns('',
 	url(r'^community', views.community_view, name='community_view'), 
-    url(r'^(?P<username>[\w\ ]+)/$', views.profile_view, name='profile_view'), 
+    url(r'^(?P<username>(.*))/$', views.profile_view, name='profile_view'), 
     url(r'^edit$', views.edit_profile_view, name='edit_profile_view'),
 )
 
