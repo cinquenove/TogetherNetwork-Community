d60901c445221e0604dab97fdf6921eda0a279a4
Lorenzo Setale <koalalorenzo@gmail.com>
2013-12-05 15:25:43 +0100

    Enabled actions in admin panel for Comment model

--- Activities/admin.py
+++ Activities/admin.py
@@ -2,7 +2,9 @@
 from django.contrib import admin 
 from .models import Location
 from .models import Activity
+from .models import Comment
 
 admin.site.register(Location)
 admin.site.register(Activity)
+admin.site.register(Comment)
 
