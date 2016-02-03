b848adcaf13eb21c4a471db401d386ac97183437
Ernesto Cinquenove <ernestocinquenove@gmail.com>
2016-02-03 18:06:40 +0100

    Added labels to activity form

--- Activities/forms.py
+++ Activities/forms.py
@@ -18,9 +18,22 @@ class ActivityForm(ModelForm):
 
             "attendees_limit",
             #"attendees",
-            "price",
+            "price"
         ]
 
+        labels = {
+            "title": "Title",
+            "description": "Description",
+            "activity_type": "Activity ype",
+
+            "time": "Time",
+            "location": "Where is the activity taking place?",
+            "photo": "Cover image",
+
+            "attendees_limit:": "Attendees Limit",
+            
+            "price": "Price",
+        }
         help_texts = {
             'price': 'Se le modalità di pagamento sono disponibili nella descrizione, in questo campo scrivi \"-1\" ',
             'location': 'Se l\'attività non ha luogo nelle case, specificare l\'indirizzo nella descrizione',
